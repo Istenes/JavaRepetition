7decd964d27e20a6aef18fd17bfec902d4a64cce
Niklas Istenes <niklas@retroid.com>
2020-10-12 15:38:57 +0200

    added working game

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,23 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Ange en siffra: ");
+        int maxNumber = 50;
+        Random random = new Random();
+        int randomNumber = random.nextInt(maxNumber);
+
+        System.out.printf("Gissa en siffra mellan 0 och %d: ", maxNumber);
         Scanner scanner = new Scanner(System.in);
-        int siffra = scanner.nextInt();
-        //System.out.println("Användaren skrev siffran: " + siffra + "!");
-        System.out.printf("Användaren skrev siffran: %d!", siffra);
+        int number = scanner.nextInt();
+
+        if (number == randomNumber) {
+            System.out.println("Grattis du gissade rätt!");
+        } else {
+            System.out.printf("Tyvärr du gissade fel :( rätt siffra var: %d", randomNumber);
+        }
+
     }
 
 }
