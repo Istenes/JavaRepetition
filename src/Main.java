58dbd67ddc7bf676dc4cba7929b2264162ffd3c6
MAUMA <maurizio.macri@hm.com>
2020-10-12 16:32:22 +0200

    z

--- src/Main.java
+++ src/Main.java
@@ -4,20 +4,23 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        int maxNumber = 50;
+        int maxNumber = 5;
         Random random = new Random();
         int randomNumber = random.nextInt(maxNumber);
-
-        System.out.printf("Gissa en siffra mellan 0 och %d: ", maxNumber);
+        System.out.println(randomNumber);
+        boolean wrong = true;
         Scanner scanner = new Scanner(System.in);
-        int number = scanner.nextInt();
 
-        if (number == randomNumber) {
-            System.out.println("Grattis du gissade rätt!");
-        } else {
-            System.out.printf("Tyvärr du gissade fel :( rätt siffra var: %d", randomNumber);
-        }
+        do {
+            System.out.printf("\nGissa en siffra mellan 0 och %d: ", maxNumber);
+            int number = scanner.nextInt();
+            if (number == randomNumber) {
+                System.out.println("Grattis du gissade rätt!");
+                wrong = false;
+            } else {
+                System.out.printf("Tyvärr du gissade fel :( rätt siffra var: %d", randomNumber);
+            }
+        } while (wrong);
 
     }
-
 }
