319b12ea0a517d306a5afde352d7bc509ee22c09
MAUMA <maurizio.macri@hm.com>
2020-10-12 14:15:13 +0200

    printf för utskrifter

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,16 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Ange en siffra: ");
+        System.out.print("Ange en siffra: ");
         Scanner scanner = new Scanner(System.in);
         int siffra = scanner.nextInt();
-        //System.out.println("Användaren skrev siffran: " + siffra + "!");
-        System.out.printf("Användaren skrev siffran: %d!", siffra);
-    }
 
+        if (siffra > 5) {
+            System.out.printf("Siffran är högre än 5. Din siffra var %d." , siffra);
+        } else if (siffra < 5) {
+            System.out.printf("Siffran är lägre än 5. Din siffra var %d.", siffra);
+        } else {
+            System.out.printf ("Siffran var precis %d.", siffra);
+        }
+    }
 }
