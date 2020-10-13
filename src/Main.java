2c1087266f4fcb0a386abcd446d0a375a2d5515a
Niklas Istenes <niklas@retroid.com>
2020-10-13 10:45:26 +0200

    Added possability to select a difficulty, tell the player how many total tries he or she had when successfully guessed the correct number

--- src/Main.java
+++ src/Main.java
@@ -4,20 +4,39 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        int maxNumber = 10;
+        Scanner scanner = new Scanner(System.in);
+        int maxNumber;
+        int numberOfTries;
+        System.out.printf("Ange svårighetsgrad\n1) Lätt\n2) Medel\n3) Svår\n");
+        int difficulty = scanner.nextInt();
+        switch (difficulty) {
+            case 1:
+                maxNumber = 5;
+                numberOfTries = 5;
+                break;
+            case 2:
+                maxNumber = 10;
+                numberOfTries = 3;
+                break;
+            case 3:
+                maxNumber = 20;
+                numberOfTries = 2;
+                break;
+            default:
+                System.out.printf("Du angav inte en siffra mellan 1 och 3, avbryter spelet.");
+                return;
+        }
         Random random = new Random();
         // Add 1 to make it between 1 and maxNumber
         int correctNumber = random.nextInt(maxNumber) + 1;
         System.out.printf("(correctNumber: %d)\n", correctNumber);
-        int numberOfTries = 3;
 
         for (int guessCount = 1; guessCount <= numberOfTries; guessCount++) {
             System.out.printf("Gissa en siffra mellan 1 och %d: ", maxNumber);
-            Scanner scanner = new Scanner(System.in);
             int number = scanner.nextInt();
             if (number == correctNumber) {
                 // Användaren har gissat rätt
-                System.out.printf("Grattis du gissade rätt! Du klarade det på %d försök", guessCount);
+                System.out.printf("Grattis du gissade rätt! Du klarade det på %d/%d försök", guessCount, numberOfTries);
                 return;
             } else if(number < correctNumber) {
                 System.out.println("Du gissade för lågt, försök igen!");
