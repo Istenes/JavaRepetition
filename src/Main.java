c400072128799cf6340e628bd3ed62c3d4debde2
MAUMA <maurizio.macri@hm.com>
2020-10-13 11:21:15 +0200

    case for letters, accepts words as difficulty

--- src/Main.java
+++ src/Main.java
@@ -5,26 +5,36 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int maxNumber;
-        int numberOfTries;
-        System.out.printf("Ange svårighetsgrad\n1) Lätt\n2) Medel\n3) Svår\n");
-        int difficulty = scanner.nextInt();
-        switch (difficulty) {
-            case 1:
-                maxNumber = 5;
-                numberOfTries = 5;
-                break;
-            case 2:
-                maxNumber = 10;
-                numberOfTries = 3;
-                break;
-            case 3:
-                maxNumber = 20;
-                numberOfTries = 2;
-                break;
-            default:
-                System.out.printf("Du angav inte en siffra mellan 1 och 3, avbryter spelet.");
-                return;
+        int maxNumber = 0;
+        int numberOfTries = 0;
+        boolean notAllowed = true;
+        while (notAllowed) {
+            System.out.printf("Ange svårighetsgrad:\n1) Lätt\n2) Medel\n3) Svår\n");
+            String difficulty = scanner.next().toLowerCase();
+            switch (difficulty) {
+                case "1":
+                case "lätt":
+                case "easy":
+                    maxNumber = 5;
+                    numberOfTries = 5;
+                    notAllowed = false;
+                    break;
+                case "2":
+                case "medel":
+                    maxNumber = 10;
+                    numberOfTries = 3;
+                    notAllowed = false;
+                    break;
+                case "3":
+                case "svår":
+                    maxNumber = 20;
+                    numberOfTries = 2;
+                    notAllowed = false;
+                    break;
+                default:
+                    System.out.printf("Du angav fel värder, försök igen.%n");
+
+            }
         }
         Random random = new Random();
         // Add 1 to make it between 1 and maxNumber
@@ -38,13 +48,12 @@ public class Main {
                 // Användaren har gissat rätt
                 System.out.printf("Grattis du gissade rätt! Du klarade det på %d/%d försök", guessCount, numberOfTries);
                 return;
-            } else if(number < correctNumber) {
+            } else if (number < correctNumber) {
                 System.out.println("Du gissade för lågt, försök igen!");
             } else if (number > correctNumber) {
                 System.out.println("Du gissade för högt, försök igen!");
             }
         }
-
 
 
         // Användaren har förlorat
