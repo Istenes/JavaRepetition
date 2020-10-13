3ec9ec515c3e515055d07fa6da9b74f26266b230
Niklas Istenes <niklas@retroid.com>
2020-10-13 09:28:15 +0200

    Made the correctNumber between 1 and 10 instead of 0 and 10

--- src/Main.java
+++ src/Main.java
@@ -6,12 +6,13 @@ public class Main {
     public static void main(String[] args) {
         int maxNumber = 10;
         Random random = new Random();
-        int correctNumber = random.nextInt(maxNumber);
+        // Add 1 to make it between 1 and maxNumber
+        int correctNumber = random.nextInt(maxNumber) + 1;
         System.out.printf("(correctNumber: %d)\n", correctNumber);
         int numberOfTries = 3;
-        
+
         for (int guessCount = 1; guessCount <= numberOfTries; guessCount++) {
-            System.out.printf("Gissa en siffra mellan 0 och %d: ", maxNumber);
+            System.out.printf("Gissa en siffra mellan 1 och %d: ", maxNumber);
             Scanner scanner = new Scanner(System.in);
             int number = scanner.nextInt();
             if (number == correctNumber) {
