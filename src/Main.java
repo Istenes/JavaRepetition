5baa3a3d58e570e90f6a1a38e50affab4dbc1102
Niklas Istenes <niklas@retroid.com>
2020-10-13 09:04:30 +0200

    Refactored some variables, added messages if the user is guessing too high or too low, tell the player what the correct number is if he looses

--- src/Main.java
+++ src/Main.java
@@ -6,19 +6,26 @@ public class Main {
     public static void main(String[] args) {
         int maxNumber = 10;
         Random random = new Random();
-        int randomNumber = random.nextInt(maxNumber);
+        int correctNumber = random.nextInt(maxNumber);
 
         int numberOfTries = 3;
         for (int guessCount = 1; guessCount <= numberOfTries; guessCount++) {
             System.out.printf("Gissa en siffra mellan 0 och %d: ", maxNumber);
             Scanner scanner = new Scanner(System.in);
             int number = scanner.nextInt();
-            if (number == randomNumber) {
+            if (number == correctNumber) {
+                // Användaren har gissat rätt
                 System.out.println("Grattis du gissade rätt!");
-            } else {
-                System.out.println("Tyvärr du gissade fel :(");
+                return;
+            } else if(number < correctNumber) {
+                System.out.println("Du gissade för lågt, försök igen!");
+            } else if (number > correctNumber) {
+                System.out.println("Du gissade för högt, försök igen!");
             }
         }
+
+        // Användaren har förlorat
+        System.out.printf("Tyvärr du förlorade, rätt svar var: %d", correctNumber);
     }
 
 }
